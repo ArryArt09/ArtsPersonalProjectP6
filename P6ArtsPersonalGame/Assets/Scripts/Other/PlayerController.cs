3817eb2d756f7e22ead7c6541fed9eb51e89febd
ArryArt09 <746785@student.dusd.net>
2026-04-22 13:06:30 -0700

    Another step
    Try adding some placeholder elements, like bckgrd

--- P6ArtsPersonalGame/Assets/Scripts/Other/PlayerController.cs
+++ P6ArtsPersonalGame/Assets/Scripts/Other/PlayerController.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public float runSpeed = 2f;
     private float runVariable;
+    public bool isTalking = false;
+    private bool canMove = true;
+
+    //Game bounds (get rid of later)
+    public float xRange = 10;
+    public float yRange = 10;
 
     //Controls
     public float horizontalInput;
@@ -23,12 +29,15 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Movement
+        if (canMove)
+        {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed * runVariable);
 
         verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed * runVariable);
-
+        }
+        
         //Run System
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -37,6 +46,33 @@ public class PlayerController : MonoBehaviour
         else
         {
             runVariable = 1f;
+        }
+
+        //Game bounds (get rid of later)
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+
+        if (isTalking)
+        {
+            canMove = false;
+        }
+        else
+        {
+            canMove = true;
         }
     }
 }
